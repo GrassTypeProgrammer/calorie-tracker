4363ee6809002b446376aa90e90d0162d158be72
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-03-10 08:21:55 +0000

    Styling the active link on the navbar

---
+++ calorie-tracker/app/navbar.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import classNames from 'classnames';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import { FaBurn } from "react-icons/fa";
+
+type LinkData = {
+    label: string,
+    href: string,
+}
+
+const Navbar = () => {
+    const currentPath = usePathname();
+
+    const links: LinkData[] = [
+        { href: '/', label: 'Home' },
+        { href: '/calories', label: 'Calories' },
+    ];
+
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+            <Link href='/'>
+                <FaBurn className='h-8 w-8' />
+            </Link>
+            <ul className='flex flex-1 justify-center space-x-6 '>
+                {links.map((item: LinkData) => {
+                    return (
+                        <li key={item.href}>
+                            <Link
+                                className={classNames(
+                                    currentPath === item.href ? 'text-zinc-900' : 'text-zinc-500',
+                                    'hover:text-zinc-800 transition-colors'
+                                )}
+                                href={item.href}
+                            >
+                                {item.label}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
