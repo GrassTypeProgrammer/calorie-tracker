1e1485f38a50a8fdd5a6f6857cd47e8b0ded5854
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2025-03-23 20:08:21 +0000

    Handle errors

--- calorie-tracker/app/items/new/page.tsx
+++ calorie-tracker/app/items/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Button, TextField } from '@radix-ui/themes';
-import React from 'react';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import axios from "axios";
 import { useRouter } from 'next/navigation';
@@ -18,25 +18,36 @@ interface ItemForm {
 const NewItemPage = () => {
   const router = useRouter();
   const { handleSubmit, register } = useForm<ItemForm>();
+  const [error, setError] = useState('');
 
   return (
+    <div className='max-w-xl'>
+      {error && 
+        <Callout.Root color='red' className='mb-2'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      }
+      <form
+        className='space-y-3'
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/foodItem', data);
+            router.push('/items');
+          } catch (error) {
+            setError('An unexpected error occurred.');
+          }
+        })}
+      >
+        <TextField.Root placeholder='Item Name' {...register('name')} />
+        {/* <TextField.Root placeholder='Weight in grams'{...register('weight', {valueAsNumber: true})} /> */}
+        <TextField.Root placeholder='Protein' {...register('protein', { valueAsNumber: true })} />
+        <TextField.Root placeholder='Carbs' {...register('carbs', { valueAsNumber: true })} />
+        <TextField.Root placeholder='Fat' {...register('fat', { valueAsNumber: true })} />
+        <TextField.Root placeholder='Calories' {...register('calories', { valueAsNumber: true })} />
+        <Button>Submit Item</Button>
+      </form>
+    </div>
 
-    <form
-      className='max-w-xl space-y-3'
-      onSubmit={handleSubmit(async (data) => {
-        console.log(data)
-        await axios.post('/api/foodItem', data);
-        router.push('/items');
-      })}
-    >
-      <TextField.Root placeholder='Item Name' {...register('name')} />
-      {/* <TextField.Root placeholder='Weight in grams'{...register('weight', {valueAsNumber: true})} /> */}
-      <TextField.Root placeholder='Protein' {...register('protein', {valueAsNumber: true})}/>
-      <TextField.Root placeholder='Carbs' {...register('carbs', {valueAsNumber: true})}/>
-      <TextField.Root placeholder='Fat' {...register('fat', {valueAsNumber: true})}/>
-      <TextField.Root placeholder='Calories' {...register('calories', {valueAsNumber: true})}/>
-      <Button>Submit Item</Button>
-    </form>
   )
 }
 
